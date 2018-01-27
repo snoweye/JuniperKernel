fabf0d16682de7e4b8e282c9c013cf3875bc412b
wccsnow <wccsnow@gmail.com>
2018-01-26 23:12:21 -0500

    zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -16,4 +16,51 @@
 # along with JuniperKernel.  If not, see <http://www.gnu.org/licenses/>.
 
 .onAttach <- function(lib, pkg) {}
-.onLoad   <- function(lib, pkg) {}
+.onLoad   <- function(lib, pkg) {
+  dn.pbdZMQ <- tools::file_path_as_absolute(
+                 system.file("./libs", package = "pbdZMQ")) 
+
+  ### For osx only.
+  if(Sys.info()[['sysname']] == "Darwin") {
+    cmd.int <- system("which install_name_tool", intern = TRUE)
+    cmd.ot <- system("which otool", intern = TRUE)
+
+    ### Get rpath from pkg's shared library.
+    fn.so <- paste(lib, "/", pkg, "/libs/", pkg, ".so", sep = "")
+    rpath <- system(paste(cmd.ot, " -L ", fn.so, sep = ""),
+                    intern = TRUE)
+
+    ### Get dylib file from rpath
+    pattern <- paste("^\\t(.*/pbdZMQ/libs/libzmq.*\\.dylib) .*$", sep = "")
+    i.rpath <- grep(pattern, rpath)
+    fn.dylib <- gsub(pattern, "\\1", rpath[i.rpath])
+
+    ### Do nothing if the dylib file exists at which rpath points.
+    ### Overwrite with one searched from path if the dylib file does not exist.
+    if(length(fn.dylib) == 1) {
+      if(!file.exists(fn.dylib)) {
+        fn <- list.files(path = dn.pbdZMQ, pattern = "libzmq.*\\.dylib")
+        new.fn.dylib <- paste(dn.pbdZMQ, "/", fn, sep = "")
+
+        cmd <- paste(cmd.int, " -change ", fn.dylib, " ", new.fn.dylib,
+                     " ", fn.so,
+                     sep = "")
+        system(cmd)
+      }
+    }
+  }
+
+  ### Load "libzmq*"
+  fn <- list.files(path = dn.pbdZMQ, pattern = "libzmq\\..*")
+  i.file <- paste(dn.pbdZMQ, "/", fn, sep = "")
+  test <- try(dyn.load(i.file, local = FALSE), silent = TRUE)
+  if(class(test) == "try-error"){
+    stop(paste("Could not load ", i.file, ":",
+               paste(test, collapse = ", "), sep = " "))
+  }
+
+  ### Load "pkg.so".
+  library.dynam(pkg, pkg, lib)
+
+  invisible()
+} # End of .onLoad().
